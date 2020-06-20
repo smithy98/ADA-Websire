b88ef755fa2aed1a5c5ba1f7b334129e6c420185
smithy98 <smithy_98@outlook.com>
2020-06-20 13:22:12 +0100

    added style to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  header: {
+    "font-family": "Constantia",
+    "font-size": 50,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <header>
       <AppBar position="static">
         <Toolbar>
-          <Typography>Away Days Apparel</Typography>
+          <Typography className={classes.header}>Away Days Apparel</Typography>
         </Toolbar>
       </AppBar>
     </header>
